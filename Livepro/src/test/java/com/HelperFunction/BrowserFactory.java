a8bde0249760183c3d5bd1313597d2d0f40bf41d
Abhishek Gupta <abhishek.replica@gmail.com>
2016-04-27 17:07:37 +0530

    Test files

--- Livepro/src/test/java/com/HelperFunction/BrowserFactory.java
+++ Livepro/src/test/java/com/HelperFunction/BrowserFactory.java
@@ -1,7 +1,12 @@
 package com.HelperFunction;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
